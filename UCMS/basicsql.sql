09a92bb10d52c63682927fd6eabafb86f8822bc6
Romaisa0157 <you@example.com>
2023-05-13 15:36:22 +0500

    added dummy data

--- UCMS/basicsql.sql
+++ UCMS/basicsql.sql
@@ -162,8 +162,32 @@ VALUES
 (3445, 'Hostel GH', 'Cleaning')
 ;
 
+-- Insert data into the complaint table
 INSERT INTO complaint (complaint_ID, issue_date, resolve_date, complaint_type, description, lodger_ID)
 VALUES
-(1, '2022-04-01', '2022-04-03', 'Hostel', 'Broken light fixture', 2021538),
-(2, '2022-05-15', '2022-05-17', 'Medical Centre', 'N/A',2990),
-(3, '2022-06-20', NULL, 'Mess', 'N/A', 2021173);
+    (12,'2023-05-01', '2023-05-05', 'Maintenance', 'Leaky faucet', 2021538),
+    (13,'2023-05-03', '2023-05-07', 'Hostel', 'Power outage', 2021173),
+    (14,'2023-05-02', NULL, 'Security', 'Unauthorized entry', 2019777)
+	(15,'2023-06-01', '2023-07-05','Maintenance', 'N/A', 2990),
+	(16,'2023-05-04', '2023-06-07','Mess', 'N/A', 2890),
+	(17,'2023-03-04', '2023-04-10','Others', 'N/A', 2876);
+
+-- Insert data into the respondent table
+INSERT INTO respondent (respondent_ID, respondent_email, respondent_password, respondent_name, assigned_complaint)
+VALUES
+    (1, 'respondent1@example.com', 'password123', 'Respondent 1', 12),
+    (2, 'respondent2@example.com', 'password456', 'Respondent 2', 13),
+    (3, 'respondent3@example.com', 'password789', 'Respondent 3', 14);
+
+-- Insert data into the admin table
+INSERT INTO admin (admin_ID,admin_email, admin_password, complaint_ID, respondent_email)
+VALUES
+    (1001,'admin1@giki.edu.pk', 'adminpassword1', 12, 'respondent1@example.com'),
+    (1002,'admin2@giki.edu.pk', 'adminpassword2', 13, 'respondent2@example.com'),
+    (1003,'admin3@giki.edu.pk', 'adminpassword3', 14, 'respondent3@example.com');
+	
+INSERT INTO resolves(res_ID, complaint_ID, status)
+VALUES
+	(1, 12, 'Resolved'),
+	(2, 13, 'Resolved'),
+	(3, 14, 'Unresolved');
